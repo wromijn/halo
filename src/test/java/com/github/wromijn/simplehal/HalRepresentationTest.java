09809bfe52a77c633d7291a34fc2118b836781df
Willem Romijn <willem.romijn@nepgroup.com>
2018-12-29 22:48:14 +0100

    added a test

---
+++ src/test/java/com/github/wromijn/simplehal/HalRepresentationTest.java
@@ -0,0 +1,17 @@
+package com.github.wromijn.simplehal;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+public class HalRepresentationTest {
+    public void testProperties() {
+        HalRepresentation representation = new HalRepresentation();
+        representation.addBoolean("boolean_value", true);
+
+        JsonNode result = new ObjectMapper().valueToTree(representation);
+        assertThat(result.get("boolean_value").booleanValue(), is(true));
+    }
+}
